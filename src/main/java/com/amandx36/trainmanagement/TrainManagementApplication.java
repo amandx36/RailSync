829cdbbeb9529b923a6b0d1d687ef20e0712abe5
amandx36 <xamandeep360@gmail.com>
2026-07-21 10:52:58 +0530

    Started the Project

---
+++ src/main/java/com/amandx36/trainmanagement/TrainManagementApplication.java
@@ -0,0 +1,13 @@
+package com.amandx36.trainmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amandx36/trainmanagement/TrainManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amandx36.trainmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
