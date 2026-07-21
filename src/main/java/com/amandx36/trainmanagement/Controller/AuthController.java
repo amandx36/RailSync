8baac8895e3cef60d1a686cea47e2229dd29d3c6
amandx36 <xamandeep360@gmail.com>
2026-07-21 19:04:11 +0530

    Added the the login layer

--- src/main/java/com/amandx36/trainmanagement/Controller/AuthController.java
+++ src/main/java/com/amandx36/trainmanagement/Controller/AuthController.java
@@ -2,13 +2,16 @@ package com.amandx36.trainmanagement.Controller;
 
 
 import com.amandx36.trainmanagement.AuthService.AuthService;
+import com.amandx36.trainmanagement.dto.reponse.AuthResponse;
 import com.amandx36.trainmanagement.dto.reponse.RegisterResponse;
+import com.amandx36.trainmanagement.dto.request.LoginRequest;
 import com.amandx36.trainmanagement.dto.request.RegisterRequest;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +29,13 @@ public class AuthController {
     ){
       RegisterResponse registerResponse = authService.register(request);
       return ResponseEntity.ok(registerResponse);
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse>login(@Valid @RequestBody LoginRequest loginRequest){
+        AuthResponse response = authService.login(loginRequest);
+        return ResponseEntity.ok(response);
     }
 
 
