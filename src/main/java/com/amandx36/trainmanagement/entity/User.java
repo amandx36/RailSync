7802f63bc32fc5daaedb1c70581960217e64759c
amandx36 <xamandeep360@gmail.com>
2026-07-21 11:19:34 +0530

    Added the repository layer

--- src/main/java/com/amandx36/trainmanagement/entity/User.java
+++ src/main/java/com/amandx36/trainmanagement/entity/User.java
@@ -18,7 +18,7 @@ public class User{
     private String firstName;
     private String lastName;
     @Enumerated(EnumType.STRING)
-    private UserRole role;m
+    private UserRole role;
     private LocalDateTime createdAt;
     private Boolean active;
 
